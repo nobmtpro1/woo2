4795f023fe0b240dd3f2403d65c4eeaa40764013
Thai Nguyen <thai.nguyen@aegona.com>
2023-05-11 20:43:25 +0700

    #

---
+++ wp-content/themes/blank/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+    <div class="row">
+        <div class="info">
+            <h3>CÔNG TY CỔ PHẦN ONOFF </h3> <br>
+            ĐKKD: 0303930269, ngày cấp: 18/08/2005 <br>
+            Sở Kế hoạch và đầu tư TP. Hồ Chí Minh <br>
+            Địa chỉ trụ sở chính: 366 Hai Bà Trưng, Phường Tân Định, Quận 1, TP Hồ Chí Minh <br>
+        </div>
+        <ul>
+            <li>
+                <h3>THÔNG TIN HỖ TRỢ </h3>
+            </li>
+            <li><a href="">Hướng dẫn thanh toán</a> </li>
+            <li><a href="">Hướng dẫn mua hàng</a></li>
+        </ul>
+        <ul>
+            <li>
+                <h3>THƯƠNG HIỆU </h3>
+            </li>
+            <li><a href="">Giới thiệu</a> </li>
+            <li><a href="">Blog</a></li>
+        </ul>
+        <ul>
+            <li>
+                <h3>Liên hệ</h3>
+            </li>
+            <li>1800 1100 (miễn phí cước gọi) </li>
+            <li>info@onoff.vn</li>
+        </ul>
+    </div>
+    <div class="copyright">
+        © 2018 ONOFF Fashion. All right reserved
+    </div>
+</footer>
+
+<?php get_footer() ?>
+</body>
+
+</html>
