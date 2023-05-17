8a582f862c77400985176e337203adf623ca6ae2
Thai Nguyen <thai.nguyen@aegona.com>
2023-05-17 21:28:27 +0700

    #

--- wp-content/themes/blank/footer.php
+++ wp-content/themes/blank/footer.php
@@ -1,35 +1,57 @@
 <footer class="footer">
     <div class="row">
         <div class="info">
-            <h3>CÔNG TY CỔ PHẦN ONOFF </h3> <br>
-            ĐKKD: 0303930269, ngày cấp: 18/08/2005 <br>
-            Sở Kế hoạch và đầu tư TP. Hồ Chí Minh <br>
-            Địa chỉ trụ sở chính: 366 Hai Bà Trưng, Phường Tân Định, Quận 1, TP Hồ Chí Minh <br>
+            <h3><?= get_theme_mod('footer_col1_title') ?></h3> <br>
+            <?= get_theme_mod('footer_col1_description') ?>
         </div>
         <ul>
             <li>
-                <h3>THÔNG TIN HỖ TRỢ </h3>
+                <h3><?= get_theme_mod('footer_col2_title') ?> </h3>
             </li>
-            <li><a href="">Hướng dẫn thanh toán</a> </li>
-            <li><a href="">Hướng dẫn mua hàng</a></li>
+            <?php for ($i = 1; $i < 10; $i++) : ?>
+                <?php
+                $link_name = get_theme_mod('footer_col2_link_name_' . $i);
+                $link = get_theme_mod('footer_col2_link_' . $i);
+                if (!$link_name && !$link) {
+                    continue;
+                }
+                ?>
+                <li><a href="<?= $link ?>"><?= $link_name ?></a> </li>
+            <?php endfor ?>
         </ul>
         <ul>
             <li>
-                <h3>THƯƠNG HIỆU </h3>
+                <h3><?= get_theme_mod('footer_col3_title') ?> </h3>
             </li>
-            <li><a href="">Giới thiệu</a> </li>
-            <li><a href="">Blog</a></li>
+            <?php for ($i = 1; $i < 10; $i++) : ?>
+                <?php
+                $link_name = get_theme_mod('footer_col3_link_name_' . $i);
+                $link = get_theme_mod('footer_col3_link_' . $i);
+                if (!$link_name && !$link) {
+                    continue;
+                }
+                ?>
+                <li><a href="<?= $link ?>"><?= $link_name ?></a> </li>
+            <?php endfor ?>
         </ul>
         <ul>
             <li>
-                <h3>Liên hệ</h3>
+                <h3><?= get_theme_mod('footer_col4_title') ?> </h3>
             </li>
-            <li>1800 1100 (miễn phí cước gọi) </li>
-            <li>info@onoff.vn</li>
+            <?php for ($i = 1; $i < 10; $i++) : ?>
+                <?php
+                $link_name = get_theme_mod('footer_col4_link_name_' . $i);
+                $link = get_theme_mod('footer_col4_link_' . $i);
+                if (!$link_name && !$link) {
+                    continue;
+                }
+                ?>
+                <li><a href="<?= $link ?>"><?= $link_name ?></a> </li>
+            <?php endfor ?>
         </ul>
     </div>
     <div class="copyright">
-        © 2018 ONOFF Fashion. All right reserved
+        <?= get_theme_mod('footer_copyright') ?>
     </div>
 </footer>
 
