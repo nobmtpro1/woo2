fac1326ae9c7800219e527513bd95839f696e397
Thai Nguyen <thai.nguyen@aegona.com>
2023-05-17 14:42:46 +0700

    #

--- wp-content/themes/blank/index.php
+++ wp-content/themes/blank/index.php
@@ -1,6 +1,6 @@
 <?php get_header() ?>
 <div class="container">
-    <h1>Newest</h1>
+    <h1>Sản phẩm mới nhất</h1>
     <?= do_shortcode('[products columns="4" orderby="id" order="DESC" visibility="visible" paginate="true" per_page="12"]') ?>
 
 </div>
